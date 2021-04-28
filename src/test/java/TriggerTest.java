752bf8783707054b03eeaa99183329168580a306
rpegorov <rostislav.egorov@gmail.com>
2021-04-28 22:30:24 +0300

    Trigger codecov

---
+++ src/test/java/TriggerTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TriggerTest {
+
+    @Test
+    public void someLogic() {
+        Assert.assertEquals(1, new Trigger().someLogic());
+    }
+}
