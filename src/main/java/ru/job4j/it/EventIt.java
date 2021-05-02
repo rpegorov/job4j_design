45e46e3783e112754007ef4fe350cc6d3a8b1b39
rpegorov <rostislav.egorov@gmail.com>
2021-05-02 08:32:35 +0300

    Создать итератор четные числа [#526163]

---
+++ src/main/java/ru/job4j/it/EventIt.java
@@ -0,0 +1,48 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EventIt implements Iterator<Integer> {
+    private final int[] data;
+    private int point;
+
+    public EventIt(int[] data) {
+        this.data = data;
+    }
+
+    /**
+     * Метод проверяет есть ли в массиве следующее четное значение.
+     * Если метод exist возвращает 0 значит значение есть.
+     */
+    @Override
+    public boolean hasNext() {
+        return exist() == 0;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[point++];
+    }
+
+/**
+ * Метод проводит поиск ячейки в которой находится четное число.
+ * При это каждый последующий поиск элемента будет уменьшать
+ * количество итераций цикла на point. Если очередное четное число
+ * найдено не будет метод вернет -1.
+ */
+    private Integer exist() {
+        var value = -1;
+        for (int index = point; index < data.length; index++) {
+            if (data[index] % 2 == 0) {
+                point = index;
+                value++;
+                break;
+            }
+        }
+        return value;
+    }
+}
