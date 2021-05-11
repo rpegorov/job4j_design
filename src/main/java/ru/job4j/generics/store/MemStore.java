30547f4631d414df93f52d96ad88cf18ac78b9b5
rpegorov <rostislav.egorov@gmail.com>
2021-05-11 08:41:35 +0300

    5.2.2. Реализовать Store<T extends Base> [#526177] add func searchId, refactoring searchId

--- src/main/java/ru/job4j/generics/store/MemStore.java
+++ src/main/java/ru/job4j/generics/store/MemStore.java
@@ -14,7 +14,7 @@ public class MemStore<T extends Base> implements Store<T> {
 
     @Override
     public boolean replace(String id, Base model) {
-        var index = mem.indexOf(id);
+        var index = searchId(id);
         if (index != -1) {
             mem.set(index, (T) model);
             return true;
@@ -24,7 +24,7 @@ public class MemStore<T extends Base> implements Store<T> {
 
     @Override
     public boolean delete(String id) {
-        var index = mem.indexOf(id);
+        var index = searchId(id);
         if (index != -1) {
             mem.remove(id);
             return true;
@@ -34,7 +34,18 @@ public class MemStore<T extends Base> implements Store<T> {
 
     @Override
     public T findById(String id) {
-        var index = mem.indexOf(id);
+        var index = searchId(id);
         return index != -1 ? mem.get(index) : null;
+    }
+
+    private int searchId(String id) {
+        var rsl = -1;
+        for (int i = 0; i < mem.size(); i++) {
+            if (mem.get(i).getId().equals(id)) {
+                rsl = i;
+                break;
+            }
+        }
+        return rsl;
     }
 }
