641d55c5fe1e227a17a50929063734e462bfa164
rpegorov <rostislav.egorov@gmail.com>
2021-05-24 10:28:32 +0300

    5. Переопределить и equals и hashCode [#1002]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -30,10 +30,10 @@ public class User {
                 user.name) && Objects.equals(birthday, user.birthday);
     }
 
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(name, children, birthday);
-//    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
 
     @Override
     public String toString() {
