4e6c2e3888d8a3d46b2345edadc9582639d09d19
rpegorov <rostislav.egorov@gmail.com>
2021-08-01 21:02:36 +0300

    2. Статистика по коллекции. [#45889] skeleton code for questions;

---
+++ src/main/java/ru/job4j/generics/question/Analyze.java
@@ -0,0 +1,10 @@
+package ru.job4j.generics.question;
+
+import ru.job4j.generics.set.Set;
+
+public class Analyze {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+        return null;
+    }
+}
