8641b7aeef7c72477b7fc1041f26fbda5215e6f1
rpegorov <rostislav.egorov@gmail.com>
2021-08-09 13:22:20 +0300

    0.1. FileOutputStream. [#252488] 1

--- src/main/java/ru/job4j/io/ResultFiles.java
+++ src/main/java/ru/job4j/io/ResultFiles.java
@@ -1,24 +1,26 @@
 package ru.job4j.io;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 public class ResultFiles {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            for (int j = 0; j <= 9; j++) {
-                try (FileOutputStream out = new FileOutputStream("result.txt")) {
-                    out.write(Integer.parseInt(.getBytes(StandardCharsets.UTF_8)));
-                } catch (IOException e) {
-                    e.printStackTrace();
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int i = 1; i <= 10; i++) {
+                for (int j = 0; j <= 9; j++) {
+                    String rsl = " ";
+                    rsl += i + "x" + j + "=" + (i * j) + "\t";
+                    rsl += System.lineSeparator();
+                    out.write(rsl.getBytes());
                 }
-//                System.out.print(i + "x" + j + "=" + (i * j) + "\t");
-//            }
-//            System.out.println();
-//        }
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
+
