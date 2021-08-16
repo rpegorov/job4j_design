c6eeba079df830012190368058824350d3a18242
rpegorov <rostislav.egorov@gmail.com>
2021-08-16 09:58:36 +0300

    0.2. FileInputStream [#4898] added EvenNumberFile classes

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try {
+            FileInputStream in = new FileInputStream("even.txt");
+            StringBuilder sb = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                sb.append((char) read);
+            }
+            String[] lines = sb.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                if (Integer.parseInt(line) % 2 == 0) {
+                    System.out.println("Even - " + line);
+                } else {
+                    System.out.println("Odd - " + line);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
