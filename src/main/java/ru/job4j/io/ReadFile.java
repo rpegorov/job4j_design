59890821e0c93ec5baf2a0102a050b464dc86764
rpegorov <rostislav.egorov@gmail.com>
2021-08-16 09:05:42 +0300

    0.2. FileInputStream [#4898] added ReadFile classes

---
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try {
+            FileInputStream in = new FileInputStream("input.txt");
+            StringBuilder sb = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                sb.append((char) read);
+        }
+            System.out.println(sb);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
