760fb3286a8720ddcce9df165eb76717b90c5a9b
rpegorov <rostislav.egorov@gmail.com>
2021-10-06 15:17:21 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,61 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+       // Метод load() - должен считать все ключи в карту values.
+        // Важно в файле могут быть пустые строки и комментарии их нужно пропускать.
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                if (line.equals("") || line.startsWith("#")) {
+                    continue;
+                }
+                try {
+                    String[] parts = line.split("=");
+                    String key = parts[0];
+                    String value = parts[1];
+                    values.put(key, value);
+                } catch (Exception e) {
+                    throw new IllegalArgumentException("Invalid file format");
+                }
+            }
+        } catch (Exception e) {
+            if (e instanceof IllegalArgumentException) {
+                throw new IllegalArgumentException(e.getMessage());
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties.txt"));
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "/Users/user/IdeaProjects/job4j_design/app.properties.txt";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.connection.username"), is("postgres"));
+        assertThat(config.value("hibernate.connection.password"), is("password"));
+    }
+}
