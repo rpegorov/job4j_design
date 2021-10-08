5c58910ba0db4eb5b6d1750315b5e8fb8688e69f
rpegorov <rostislav.egorov@gmail.com>
2021-10-08 08:26:03 +0300

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,33 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Analysis {
+    public void unavailable(String source, String target) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+            try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+                List<String[]> list = in.lines().map(n -> n.split(" ")).collect(Collectors.toList());
+                String startTimeFail = null;
+                String stopTimeFail;
+                for (String[] time : list) {
+                    if (startTimeFail == null && (time[0].equals("400") || time[0].equals("500"))) {
+                        startTimeFail = time[1];
+                    }
+                    if (startTimeFail != null && (time[0].equals("200") || time[0].equals("300"))) {
+                        stopTimeFail = time[1];
+                        out.println(startTimeFail + ";" + stopTimeFail);
+                        startTimeFail = null;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Analysis().unavailable("serverLog.txt", "target.txt");
+    }
+}
