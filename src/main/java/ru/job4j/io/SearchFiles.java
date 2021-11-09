3afbd585e3168aa8b45650771c478088b370a77c
rpegorov <rostislav.egorov@gmail.com>
2021-11-09 14:04:03 +0300

    4.1. Сканирование файловой системы. [#106929] v2

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -5,38 +5,45 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
 import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class SearchFiles implements FileVisitor<Path> {
-    public SearchFiles(Predicate<Path> condition) {
 
+    private final Predicate<Path> condition;
+    List<Path> paths = new ArrayList<>();
+
+    SearchFiles(Predicate<Path> condition) {
+        this.condition = condition;
     }
 
     public List<Path> getPaths() {
-        return null;
+        return paths;
     }
 
     @Override
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-        return null;
+        return CONTINUE;
     }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        System.out.println(file.toAbsolutePath());
+        if (condition.test(file)) {
+            paths.add(file);
+        }
         return CONTINUE;
     }
 
     @Override
     public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-        return null;
+        return CONTINUE;
     }
 
     @Override
     public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-        return null;
+        return CONTINUE;
     }
 }
