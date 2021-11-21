8506265f049b22247a2a698489b4ed6cbaaada84
rpegorov <rostislav.egorov@gmail.com>
2021-11-21 20:00:22 +0300

    4.2. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/DuplicatesVisitor.java
@@ -0,0 +1,39 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    Map<FileProperty, List<Path>> files = new HashMap<>();
+
+    public List<Path> getDuplicates() {
+        List<Path> pathList = new ArrayList<>();
+        files.values()
+                .stream()
+                .filter(e -> e.size() > 1)
+                .forEach(pathList::addAll);
+        return pathList;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty fP = new FileProperty(attrs.size(), file.getFileName().toString());
+        if (files.containsKey(fP)) {
+            List<Path> duplicates = new ArrayList<>(files.get(fP));
+            duplicates.add(file);
+            files.put(fP, duplicates);
+        } else {
+            List<Path> lst = new ArrayList<>();
+            lst.add(file);
+            files.put(fP, lst);
+        }
+        return super.visitFile(file, attrs);
+    }
+}
