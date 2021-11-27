6acdcb0038cd52b2b50a2fc23bf9629c20d11d89
rpegorov <rostislav.egorov@gmail.com>
2021-11-27 13:36:01 +0300

    5. Валидация параметров запуска. [#246865] refactor - added validate param root did and file extension

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,8 +9,14 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith("txt")).forEach(System.out::println);
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        Path start = Paths.get(args[0]);
+        if (args[1] == null) {
+            throw new IllegalArgumentException("File extension is null. Specify the extension");
+        }
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
