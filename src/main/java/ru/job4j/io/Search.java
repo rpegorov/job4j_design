a2142afd11297b1bbe53af8c506d9f2113d62834
rpegorov <rostislav.egorov@gmail.com>
2021-11-29 09:59:16 +0300

    5. Валидация параметров запуска. [#246865 #233298] Out the validation

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -2,6 +2,7 @@ package ru.job4j.io;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,20 +11,28 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
-    public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+
+    private static boolean isValid(String @NotNull [] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Invalid set arguments");
         }
-        Path start = Paths.get(args[0]);
-        if (args[1] == null) {
-            throw new IllegalArgumentException("File extension is null. Specify the extension");
+        File file = Path.of(args[0]).toFile();
+        if (!file.exists() || !file.isDirectory()) {
+            throw new IllegalArgumentException(
+                    "Not search file or Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
         }
-        search(start, p -> p.toFile().getName().endsWith("." + args[1])).forEach(System.out::println);
+        return true;
     }
 
     public static @NotNull List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void main(String[] args) throws IOException {
+        isValid(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith("." + args[1])).forEach(System.out::println);
     }
 }
