a9f0f224bd27a4a80820994b02865fb0c6049cb5
rpegorov <rostislav.egorov@gmail.com>
2021-12-06 10:35:50 +0300

    5.1. Именованные аргументы [#295518 #233317]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -3,7 +3,6 @@ package ru.job4j.io;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Stream;
 
 public class ArgsName {
 
@@ -25,16 +24,15 @@ public class ArgsName {
         String key;
         String value;
         String[] param;
-        if (!(args.length == 0)) {
-            isValid(args);
-            for (String s : args) {
-                param = s.split("=");
-                key = param[0].replace("-", "");
-                value = param[1];
-                values.put(key, value);
-            }
-        } else {
+        if (args.length == 0) {
             throw new IllegalArgumentException("Invalid set arguments");
+        }
+            isValid(args);
+        for (String s : args) {
+            param = s.split("=");
+            key = param[0].replace("-", "");
+            value = param[1];
+            values.put(key, value);
         }
     }
 
@@ -45,8 +43,8 @@ public class ArgsName {
     }
 
     public static void main(String[] args) {
-        ArgsName jvm = ArgsName.of(new String[]{"-Xml=512", "-encoding=UTF-8"});
-        System.out.println(jvm.get("Xml"));
+        ArgsName jvm = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(jvm.get("encoding"));
 
         ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
         System.out.println(zip.get("out"));
