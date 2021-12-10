20a7f5339958a6bd8c66ed1b7995ec651d30ede0
rpegorov <rostislav.egorov@gmail.com>
2021-12-10 15:38:00 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/socket/EchoServer.java
+++ src/main/java/ru/job4j/socket/EchoServer.java
@@ -12,18 +12,31 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String stop = in.readLine().substring(10).split(" ")[0];
-                    if (!"bye".equals(stop)) {
-                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                        for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                            System.out.println(str);
-                        }
-                        out.flush();
-                    } else {
-                        server.close();
+                    String word = in.readLine().substring(10).split(" ")[0];
+                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
+                        serverMSG(server, out, word);
+                        System.out.println(str);
                     }
+                    out.flush();
                 }
             }
+        }
+    }
+
+    private static void serverMSG(ServerSocket server, OutputStream out, String word) throws IOException {
+        String msg;
+        if ("hello".equals(word)) {
+            msg = "hello )";
+            out.write("HTTP/1.1 200 OK\r\n".getBytes());
+            out.write(msg.getBytes());
+        }
+        if ("exit".equals(word)) {
+            server.close();
+        }
+        if (!"hello".equals(word) && !"exit".equals(word)) {
+            msg = "what?)";
+            out.write("HTTP/1.1 200 OK\r\n".getBytes());
+            out.write(msg.getBytes());
         }
     }
 }
