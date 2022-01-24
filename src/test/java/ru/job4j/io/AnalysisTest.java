3f46f8d50f77c3917a2dfe93f3e038b2f3c1c7f1
rpegorov <rostislav.egorov@gmail.com>
2022-01-24 08:42:09 +0300

    refactor

--- src/test/java/ru/job4j/io/AnalysisTest.java
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -4,9 +4,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import ru.job4j.io.readAndWrite.Analysis;
-
 import java.io.*;
-
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
@@ -17,7 +15,7 @@ public class AnalysisTest {
     @Test
     public void when4Down() throws IOException {
         File target = folder.newFile("target.txt");
-        Analysis.unavailable("./serverLog.txt", target.getAbsolutePath());
+        Analysis.unavailable("./target/fileHeap/serverLog.txt", target.getAbsolutePath());
         StringBuilder rsl = new StringBuilder();
         try (BufferedReader in = new BufferedReader(new FileReader(target))) {
             in.lines().forEach(rsl::append);
@@ -28,7 +26,7 @@ public class AnalysisTest {
     @Test
     public void whenNotDown() throws IOException {
         File target = folder.newFile("target.txt");
-        Analysis.unavailable("./serverLogNotDown.txt", target.getAbsolutePath());
+        Analysis.unavailable("./target/fileHeap/serverLogNotDown.txt", target.getAbsolutePath());
         StringBuilder rsl = new StringBuilder();
         try (BufferedReader in = new BufferedReader(new FileReader(target))) {
             in.lines().forEach(rsl::append);
@@ -39,7 +37,7 @@ public class AnalysisTest {
     @Test
     public void whenDataFileNotDown() throws IOException {
         File target = folder.newFile("target.txt");
-        Analysis.unavailable("./serverLogNotDown.txt", target.getAbsolutePath());
+        Analysis.unavailable("./target/fileHeap/serverLogNotDown.txt", target.getAbsolutePath());
         StringBuilder rsl = new StringBuilder();
         try (BufferedReader in = new BufferedReader(new FileReader(target))) {
             in.lines().forEach(rsl::append);
