5a2c0e6106847ae34bccbc0e6852e14214c2ee45
rpegorov <rostislav.egorov@gmail.com>
2022-01-25 09:16:13 +0300

    3. Фильтры [#1732 #253806] delete other scripts, refactor search product type where name product сыр

--- scriptsSQL/grocery_store/grocery_store.sql
+++ scriptsSQL/grocery_store/grocery_store.sql
@@ -1,4 +1,25 @@
 
+--create database
+create database grocery_store_db;
+
+
+
+--create table
+
+create table type(
+                     id serial primary key,
+                     name text
+);
+
+create table product(
+                        id serial primary key,
+                        name text,
+                        expired_date date,
+                        price float4,
+                        type_id int references type(id)
+);
+
+
 --inset table
 insert into type (name)
 VALUES ('Молочные'),
@@ -39,3 +60,54 @@ values ('Сухарики ржаные', '18/04/2022', 35, 4),
        ('Сухарики квадратные', '23/05/2022', 40, 4),
        ('Таранка с перцем', '23/05/2022', 77, 4);
 
+
+--selects
+
+--Просроченные продукты
+select p.name, p.expired_date
+from product p
+where current_date > p.expired_date;
+
+--Показать самый дорогой продукт
+select *
+from product
+where price = (select max(price) from product);
+
+-- Тип и общее кол-во продуктов по типу
+select t.name, count(p.name)
+from product p
+         join type t on p.type_id = t.id
+group by t.name;
+
+-- Показать все сыры
+select t.name, p.name
+from type t
+join product p on t.id = p.type_id
+where t.name LIKE '%Сыр%';
+
+--Показать все продукты из типа Сыры и Молочные
+select p.name, p.type_id
+from product p
+where type_id
+          in (select type.id
+              from type
+              where name in ('Сыры', 'Молочные'));
+
+--Показать типы продуктов которых меньше 4
+select t.name, count(type_id)
+from type t
+         join product p on t.id = p.type_id
+group by t.name
+having count(t.name) < 4;
+
+--Показать типы продуктов которых больше 10
+select t.name, count(type_id)
+from type t
+         join product p on t.id = p.type_id
+group by t.name
+having count(t.name) > 10;
+
+-- Показать имена продуктов и имена типов (вывести все)
+select p.name, t.name
+from product p
+join type t on t.id = p.type_id;
--- scriptsSQL/grocery_store/init.sql
+++
@@ -1,4 +0,0 @@
-
---create database
-create database grocery_store_db;
-
